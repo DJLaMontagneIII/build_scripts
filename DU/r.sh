d4afd1c44a81af0cb5c5e25992a0f12b4bde5fec
David LaMontagne <DJLaMontagneIII@Gmail.com>
2016-10-16 20:20:22 -0400

    Update r.sh

--- DU/r.sh
+++ DU/r.sh
@@ -7,45 +7,45 @@ txtrst=$(tput sgr0)             # Reset
 
 MODE="$1"
 
+echo -e "${bldgrn}Build script initiated ${txtrst}"
+
+# JACK and NINJA workarounds
+echo -e "${bldgrn}Starting JACK and NINJA workarounds ${txtrst}"
+#export USE_NINJA=false
+rm -rf ~/.jack*
+export ANDROID_JACK_VM_ARGS="-Xmx6g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
+out/host/linux-x86/bin/jack-admin kill-server
+out/host/linux-x86/bin/jack-admin start-server
+
 # Reading mode
 if [ ! -z $MODE ]; then
     if [ $MODE == "c" ]; then
-       echo -e "${bldgrn}Building clean ${txtrst}"
+       echo -e "${bldgrn}Compiling clean ${txtrst}"
        make clobber;
-       rm s.txt;
     else
-        echo -e "${bldgrn}Unset release build flag ${txtrst}"
-        unset IS_RELEASED_BUILD
+        echo -e "${bldgrn}Compiling without cleaning out ${txtrst}"
     fi
 
     if [ $MODE == "d" ]; then
-       echo -e "${bldgrn}Building dirty ${txtrst}"
+       echo -e "${bldgrn}Compiling dirty ${txtrst}"
        rm out/target/product/shamu/system/build.prop;
        rm -rf out/target/product/shamu/obj/KERNEL_OBJ;
        rm out/target/product/shamu/*.zip;
        rm out/target/product/shamu/*.img;
        rm out/target/product/shamu/*.md5sum;
-       rm s.txt;
     fi
 else
-    echo -e "${bldgrn}Unset release build flag ${txtrst}"
-    unset IS_RELEASED_BUILD
-    rm s.txt;
+    echo -e "${bldgrn}Compiling without cleaning out ${txtrst}"
 fi
 
 # Setup environment
-echo -e "${bldgrn}Setting up build environment ${txtrst}"
+echo -e "${bldgrn}Starting build environment ${txtrst}"
 . build/envsetup.sh
 export USE_CCACHE=1
 
-# Temp fix for jack heap size error
-export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m"
-out/host/linux-x86/bin/jack-admin kill-server
-out/host/linux-x86/bin/jack-admin start-server
-
 # Start compilation
-echo -e "${bldgrn}Starting Dirty Unicorns build for shamu ${txtrst}"
-echo shamu build started > s.txt
+echo -e "${bldgrn}Starting DU build for shamu ${txtrst}"
 lunch du_shamu-userdebug
-mka bacon
-echo shamu build complete > s.txt
+make bacon
+
+echo -e "${bldgrn}Build script completed ${txtrst}"
